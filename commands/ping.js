dfb69624266945dda1f3eb6e02dc442bd85062f8
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2025-04-11 14:32:09 -0500

    alert user of upgrades being unlocked

--- commands/ping.js
+++ commands/ping.js
@@ -24,33 +24,54 @@ module.exports = {
 	},
     buttons: {
         "again": (async interaction => {
-            let ping = interaction.client.ws.ping;
-
-            const again = new ButtonBuilder()
-                .setCustomId('ping:again')
-                .setLabel('ping again!')
-                .setStyle(ButtonStyle.Secondary);
-            const row = new ActionRowBuilder()
-                .addComponents(again);
-
-            if (ping === -1) {
-                await interaction.update({
-                    content: `${pingMessages(ping, { user: interaction.user })}`,
-                    components: [row]
-                })
-            }
-
-            ping += Math.round(Math.random()*MAX_PING_OFFSET*2) - MAX_PING_OFFSET;
-            const [playerProfile, _created] = await database.Player.findOrCreate({ where: { userId: interaction.user.id } })
-            playerProfile.score += ping;
-            playerProfile.clicks += 1;
-            // do upgrades stuff here
-            await playerProfile.save();
-
-            await interaction.update({
-                content: `${pingMessages(ping, { user: interaction.user, score: playerProfile.score, clicks: playerProfile.clicks })}\n\`${score} pts\`}`,
-                components: [row]
-            });
+            await ping(interaction,false)
         })
     }
 };
+
+async function ping(interaction, isSuper) {
+    let ping = interaction.client.ws.ping;
+
+    const again = new ButtonBuilder()
+        .setCustomId('ping:again')
+        .setLabel('ping again!')
+        .setStyle(ButtonStyle.Secondary);
+    const row = new ActionRowBuilder()
+        .addComponents(again);
+
+    if (ping === -1) {
+        return await interaction.update({
+            content: `${pingMessages(ping, { user: interaction.user })}`,
+            components: [row]
+        })
+    }
+
+    ping += Math.round(Math.random()*MAX_PING_OFFSET*2) - MAX_PING_OFFSET;
+    const [playerProfile, _created] = await database.Player.findOrCreate({ where: { userId: interaction.user.id } })
+    playerProfile.score += ping;
+    playerProfile.clicks += 1;
+    // do upgrades stuff here
+    await playerProfile.save();
+
+    if (playerProfile.clicks === 350) {
+        again.setDisabled(true);
+        const disabledRow = new ActionRowBuilder().addComponents(again);
+
+        await interaction.update({
+            content: `${pingMessages(ping, { user: interaction.user, score: playerProfile.score, clicks: playerProfile.clicks })}
+            \`${score} pts\`}
+            you have a lot of pts... why don't you go spend them over in </upgrade:[ID]>?`, // TODO: change to dynamically use ID
+            components: [disabledRow]
+        })
+        
+        await new Promise(resolve => setTimeout(resolve,1500)) // wait 1.5s
+        return await interaction.update({
+            components: [row]
+        })
+    }
+
+    await interaction.update({
+        content: `${pingMessages(ping, { user: interaction.user, score: playerProfile.score, clicks: playerProfile.clicks })}\n\`${score} pts\`}`,
+        components: [row]
+    });
+}
