2e2d6f6bba6d836f44e61be6a6c58dc794290578
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2025-04-16 14:53:31 -0500

    leaderboard ?

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -7,6 +7,31 @@ module.exports = {
         .setDescription('check who\'s best')
         .setContexts(InteractionContextType.BotDM, InteractionContextType.Guild, InteractionContextType.PrivateChannel),
     async execute(interaction) {
+        let description = "*leaderboard updates every minute*\n";
+        const lbPlayers = await database.LeaderboardPlayer.findAll({
+            order: [
+                sequelize.fn(sequelize.col('position'), "DESC")
+            ]
+        });
+
+        const leaderboardEmojis = [
+            "🥇",
+            "🥈",
+            "🥉",
+            "🎗",
+        ]
         
+        for (player of lbPlayers) {
+            const puser = await interaction.client.users.fetch(player.userId)
+            description += 
+`
+${leaderboardEmojis[Math.max(leaderboardEmojis.length, player.position)]}**${puser.username}** - \`${score} pts\` total`
+        }
+
+        const embed = new EmbedBuilder()
+            .setTitle("leaderboard")
+            .setColor('#9c8e51')
+            .setDescription(description)
+        await interaction.reply({ embeds: [embed] })
     }
 }
--- events/clientStart.js
+++ events/clientStart.js
@@ -1,8 +1,33 @@
-const { Events, } = require('discord.js')
+const { Events } = require('discord.js')
+const database = require('./../helpers/database.js');
+const sequelize = require('sequelize');
 
 module.exports = {
     name: Events.ClientReady,
     async execute(client) {
         console.log("goood morning!")
+        updateLeaderboard()
+        setInterval(updateLeaderboard, 60 * 1000)
+    },
+    once: true,
+}
+
+async function updateLeaderboard() {
+    await database.LeaderboardPlayer.drop();
+    const topPlayers = await database.Player.findAll({
+        order: [
+            sequelize.fn(sequelize.col('totalScore'), "DESC")
+        ],
+        limit: 10,
+    })
+
+    let pos = 0;
+    for (player of topPlayers) {
+        pos++;
+        await database.LeaderboardPlayer.create({
+            userId: player.userId,
+            score: player.totalScore,
+            position: pos,
+        })
     }
 }
