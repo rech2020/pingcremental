c2ced4b73dfbdcf78aab977b943d61c891269635
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2025-04-18 15:24:39 -0500

    document more stats

--- commands/stats.js
+++ commands/stats.js
@@ -8,10 +8,15 @@ module.exports = {
         .setContexts(InteractionContextType.BotDM, InteractionContextType.Guild, InteractionContextType.PrivateChannel),
     async execute(interaction) {
         const response = `
+__**global**__
 ${await database.Player.count()} people have pinged at least once
 ${await database.Player.sum('totalScore')} total pts have been gained
 ${await database.Player.sum('score')} pts are currently owned
-${await database.Player.sum('clicks')} pings have been dealt with`;
-        await interaction.reply(`some stats? sure!\n${response}`);
+${await database.Player.sum('clicks')} pings have been dealt with
+__**personal**__
+${(await database.Player.findByPk(interaction.user.id)).clicks} total pings
+${(await database.Player.findByPk(interaction.user.id)).totalScore} total pts
+`;
+        await interaction.reply(`${response}`);
     }
 }
