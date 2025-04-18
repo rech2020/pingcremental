0f9a18c3c91911bcf70e097ab0c0b8138f517c6c
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2025-04-18 15:36:04 -0500

    nvm this didn't work, i'll re-add later i guess

--- helpers/apiConstants.js
+++
@@ -1,7 +0,0 @@
-// a global list of api returns for things that don't need to be called multiple times
-
-const githubContributors = await fetch('https://api.github.com/repos/monkeyswithpie/pingcremental/contributors')
-
-module.exports = {
-    githubContributors
-}
