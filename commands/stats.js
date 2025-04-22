deba57eb2b637b9d1eab45231c4d28322914b203
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2025-04-21 21:32:18 -0500

    /stats refresh

--- commands/stats.js
+++ commands/stats.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, InteractionContextType } = require('discord.js');
-const database = require('./../helpers/database.js')
+const database = require('./../helpers/database.js');
+const { buttons } = require('./feedback.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,7 +8,19 @@ module.exports = {
         .setDescription('numbers and stuff')
         .setContexts(InteractionContextType.BotDM, InteractionContextType.Guild, InteractionContextType.PrivateChannel),
     async execute(interaction) {
-        const response = `
+        const response = await getMessage(interaction);
+        await interaction.reply(response);
+    },
+    buttons: {
+        refresh: (async interaction => {
+            await interaction.update(await getMessage(interaction));
+        })
+    },
+}
+
+async function getMessage(interaction) {
+    return {
+        content: `
 __**global**__
 ${await database.Player.count()} people have pinged at least once
 ${await database.Player.sum('totalScore')} total pts have been gained
@@ -16,7 +29,14 @@ ${await database.Player.sum('clicks')} pings have been dealt with
 __**personal**__
 ${(await database.Player.findByPk(interaction.user.id)).clicks} total pings
 ${(await database.Player.findByPk(interaction.user.id)).totalScore} total pts
-`;
-        await interaction.reply(`${response}`);
-    }
+`, components: [
+            new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('stats:refresh')
+                        .setLabel('refresh')
+                        .setStyle(ButtonStyle.Secondary)
+                )
+        ]
+    };
 }
