3bb2f9f40e21fc8ac47f4cea716f7b5be254efaa
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2025-04-21 22:11:35 -0500

    use ceil() correctly

--- helpers/formatNumber.js
+++ helpers/formatNumber.js
@@ -13,7 +13,7 @@ function formatNumber(num, shortHand = false) {
     const suffixes = ['', 'K', 'M', 'B', 'T', 'Qa', 'Qi', 'Sx', 'Sp', 'Oc', 'No', 'Dc', 'Ud', 'Dd', 'Td', 'Qd', 'Qn'];   
     const suffixIndex = Math.floor((numStr.length - 1) / 3); // determine the suffix index based on the length of the number
 
-    const baseNum = Math.ceil((num / Math.pow(10, suffixIndex * 3))).toFixed(2); // divide by 1000^suffixIndex and round to 2 decimal places
+    const baseNum = (Math.ceil((num * 1000 / Math.pow(10, suffixIndex * 3))) / 1000).toFixed(2); // divide by 1000^suffixIndex and round to 2 decimal places
 
     return baseNum + suffixes[suffixIndex]; // return the number with the appropriate suffix
 }
