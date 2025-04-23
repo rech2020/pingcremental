719a8361d62f78848f05eb49ea9e8d1452e245ca
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2025-04-22 21:08:17 -0500

    add development execute because sometimes i just want to run a thing

--- commands/development.js
+++ commands/development.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, MessageFlags, InteractionContextType } = require('discord.js');
 const { ownerId } = require('./../config.json');
+const database = require('./../helpers/database.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -15,6 +16,16 @@ module.exports = {
             subcommand
                 .setName('pull')
                 .setDescription('pull the latest changes from the repository')
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('execute')
+                .setDescription('run the javascript code in the input')
+                .addStringOption(option =>
+                    option.setName('code')
+                        .setDescription('code to run')
+                        .setRequired(true)
+                )
         ),
     async execute(interaction) {
         if (interaction.user.id !== ownerId) {
@@ -36,6 +47,24 @@ module.exports = {
                 console.log(`stderr: ${stderr}`);
                 interaction.editReply(`done!\nstdout: ${stdout}\nstderr: ${stderr}`);
             });
+        } else if (interaction.options.getSubcommand() === 'execute') {
+            const code = interaction.options.getString('code');
+            let result;
+            try {
+                await interaction.reply({ content: 'running code...', flags: MessageFlags.Ephemeral });
+                result = await eval(`async (params) => { ${code} }`)({ database, interaction });
+                if (result instanceof Promise) {
+                    result = await result;
+                }
+            } catch (error) {
+                return await interaction.editReply({ content: `error happened D: | ${error}`, flags: MessageFlags.Ephemeral });
+            }
+            if (typeof result === 'object') {
+                result = JSON.stringify(result, null, 2);
+            } else if (typeof result === 'string') {
+                result = result.replace(/`/g, '\\`');
+            }
+            await interaction.editReply({ content: `done! \`\`\`js\n${result}\n\`\`\``, flags: MessageFlags.Ephemeral });
         }
     }
 }
