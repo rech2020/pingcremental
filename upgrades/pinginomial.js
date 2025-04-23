ecddb5553c78098d5461009d8b6006a8097fe6a6
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2025-04-22 20:07:00 -0500

    scale pinginomial much faster

--- upgrades/pinginomial.js
+++ upgrades/pinginomial.js
@@ -2,7 +2,7 @@ const UpgradeTypes = require('../helpers/upgradeEnums.js');
 
 module.exports = {
     getPrice(currentLevel) {
-        return Math.round(100*(3**currentLevel))
+        return Math.round(100*((3+(currentLevel*0.1))**currentLevel))
     },
     getDetails() {
         return {
