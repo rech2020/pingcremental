e8108bd7b4751cea60885dab94811f6af56994b0
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2025-04-25 14:30:22 -0500

    *actually* fix extra 0s

--- helpers/formatNumber.js
+++ helpers/formatNumber.js
@@ -2,7 +2,6 @@ function formatNumber(num, shortHand = false, decimalPlaces = 2) {
     if (num === null || num === undefined) return '0'; // handle null or undefined values
 
     const numStr = num.toString();
-    decimalPlaces = Math.min(numStr.length,decimalPlaces);
 
     const suffixes = ['', 'K', 'M', 'B', 'T', 'Qa', 'Qi', 'Sx', 'Sp', 'Oc', 'No', 'Dc', 'Ud', 'Dd', 'Td', 'Qd', 'Qn', 'Vg', 'Tg', 'Qvg', 'Qtg', 'Pnt', 'Sxt', 'Spt', 'Oct', 'Nnt', 'Dct']; //no one will get to these but better save than sorry :3  
 
@@ -22,6 +21,7 @@ function formatNumber(num, shortHand = false, decimalPlaces = 2) {
     }
 
     const suffixIndex = Math.floor((numStr.length - 1) / 3); 
+    decimalPlaces = Math.max(suffixIndex*3, decimalPlaces)
 
     const baseNum = (Math.ceil((num * (10**(decimalPlaces+1)) / Math.pow(10, suffixIndex * 3))) / (10**(decimalPlaces+1))).toFixed(decimalPlaces); 
 
